b410128103c65ee570bbac653e7da59f27e650b1
frenna <sambudwilowitz@gmail.com>
2024-06-27 01:19:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,16 @@ if __name__ == "__main__":
     hillclimber_trajects, hillclimber_score = hillclimber.hill_climber(netwerk_nationaal, max_time=120)
     visualisatie.visualisatie(hillclimber_trajects, "data/kaarten/netherlands_.geojson")
 
+    # Depth First Search algorithm
+    dfs_algoritme = depth_first.DepthFirst(netwerk_nationaal, max_time=120)
+    dfs_solution, dfs_score = dfs_algoritme.search()
+    visualisatie.visualisatie(dfs_solution.trajects, "data/kaarten/netherlands.geojson")
+
+    # Breadth First Search algorithm
+    bfs_algoritme = breadth_first.BreadthFirst(netwerk_nationaal, max_time=120)
+    bfs_solution, bfs_score = bfs_algoritme.search()
+    visualisatie.visualisatie(bfs_solution.trajects, "data/kaarten/netherlands.geojson")
+
     # Scores
     random_scores = []
     for _ in range(100):
@@ -34,6 +44,22 @@ if __name__ == "__main__":
         hillclimber_scores.append(hillclimber_score)
     
     visualisatie.histogram(hillclimber_scores, 'hillclimber algorithm')
+    
+    depth_scores = []
+    for _ in range(100):
+        depth_algoritme = depth_first.DepthFirst(netwerk_nationaal, max_time=120)
+        depth_solution, depth_score = depth_algoritme.search()
+        depth_scores.append(depth_score)
+
+    visualisatie.histogram(depth_scores, 'Depth First Algorithm')
+
+    breadth_scores = []
+    for _ in range(100):
+        breadth_algoritme = breadth_first.BreadthFirst(netwerk_nationaal, max_time=120)
+        breadth_solution, breadth_score = breadth_algoritme.search()
+        breadth_scores.append(bfs_score)
+
+    visualisatie.histogram(breadth_scores, 'Breadth First Algorithm')
 
 
 
